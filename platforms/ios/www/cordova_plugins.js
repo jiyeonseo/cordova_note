c3205cfc6fd96d8ddf0ac4f1d9e33810db42687f
Cheese.cake <cheese.cake@daumkakao.com>
2015-01-31 20:53:30 +0900

    it works

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -6,12 +6,20 @@ module.exports = [
         "clobbers": [
             "SQLitePlugin"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.brodysoft.sqlitePlugin": "1.0.3"
+    "com.brodysoft.sqlitePlugin": "1.0.3",
+    "org.apache.cordova.dialogs": "0.2.11"
 }
 // BOTTOM OF METADATA
 });
--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,46 +1,118 @@
 function ListController($scope){
-
-  $scope.noteList = [];
+    $scope.noteList = [];
 
     $scope.add = function(){
-
         var inputValue = $scope.input;
         $scope.input = "";
+        dbInsert(inputValue);
+//        var note = {
+//            "title" : inputValue
+//        }
+//        $scope.noteList.push(note);
+        dbSelect();
 
-        var note = {
-            "title" : inputValue
-        }
-
-        $scope.noteList.push(note);
     }
     $scope.delete = function(index){
-        $scope.noteList.splice(index, 1);
+        var id = $scope.noteList[index].id;
+//        $scope.noteList.splice(index, 1);
+        alert(id);
+
+        dbDelete(id);
     }
+
+    var db;
+
+    function dbConnection(){
+        db = window.sqlitePlugin.openDatabase({name: "my.db"});
+        db.transaction(function(tx) {
+            tx.executeSql('DROP TABLE IF EXISTS note');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS note (id integer primary key autoincrement, data text)');
+        });
+
+    }
+
+    function dbSelect(){
+        db.transaction(function(tx) {
+            tx.executeSql("select * from note;", [], function(tx, res) {
+                var list = [];
+                for(var i=0; i<res.rows.length; i++){
+                    var note = {
+                        "id" : res.rows.item(i).id,
+                        "title" :res.rows.item(i).data
+                    }
+                    list.push(note);
+                }
+                $scope.$apply(function(){
+                    $scope.noteList = list;
+                });
+
+//                console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+//                console.log("res.rows.item(0).cnt: " + res.rows.item(0).data + " -- should be 1");
+            });
+        });
+
+    }
+
+    function dbInsert(data){
+        db.transaction(function(tx) {
+            tx.executeSql("INSERT INTO note (data) VALUES (?)", [data], function(tx, res) {
+                console.log("insertId: " + res.insertId + " -- probably 1");
+                console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+//                dbSelect();
+            }, function(e) {
+                alert("err "+ e.message);
+                console.log("ERROR: " + e.message);
+            });
+        });
+
+    }
+
+    function dbDelete(id){
+        db.transaction(function(tx) {
+            tx.executeSql("DELETE FROM note WHERE id=?", [id], function(tx, res) {
+                console.log("insertId: " + res.insertId + " -- probably 1");
+                console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+                dbSelect();
+            }, function(e) {
+                console.log("ERROR: " + e.message);
+            });
+        });
+
+
+    }
+    var app = {
+        // Application Constructor
+        initialize: function() {
+            this.bindEvents();
+        },
+        // Bind Event Listeners
+        //
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        bindEvents: function() {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        },
+        // deviceready Event Handler
+        //
+        // The scope of 'this' is the event. In order to call the 'receivedEvent'
+        // function, we must explicitly call 'app.receivedEvent(...);'
+        onDeviceReady: function() {
+            app.receivedEvent('deviceready');
+            //
+
+//            navigator.notification.confirm("msg",
+//                function (a,b) {
+//                    alert(a,b);
+//                },
+//                "title",["111", "2222"]);
+
+        },
+        // Update DOM on a Received Event
+        receivedEvent: function(id) {
+            dbConnection();
+        }
+    };
+
+    app.initialize();
 
 }
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-
-    }
-};
-
-app.initialize();
